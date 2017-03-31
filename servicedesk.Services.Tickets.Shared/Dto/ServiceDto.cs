4da3ae8cbb991b9eec5a746fc56ea9785a90ef4e
SergeyDushkin <leirbythe@gmail.com>
2017-03-31 15:16:42 +0300

    add Code to ServiceDto

--- servicedesk.Services.Tickets.Shared/Dto/ServiceDto.cs
+++ servicedesk.Services.Tickets.Shared/Dto/ServiceDto.cs
@@ -8,5 +8,6 @@ namespace servicedesk.Services.Tickets.Shared.Dto
         public string Resource { get; set; }
         public Guid ReferenceId { get; set; }
         public string Name { get; set; }
+        public string Code { get; set; }
     }
 }
